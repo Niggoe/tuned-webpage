cfd48af99b0cc52672e05b9ad001fc25a3310aae
Nikolas Gross <niko.gross@gmail.com>
2022-12-30 23:01:26 +0100

    removed dist dir

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const path = require('path')
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
-  distDir: 'build',
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
